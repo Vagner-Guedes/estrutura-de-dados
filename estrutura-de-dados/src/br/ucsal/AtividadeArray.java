6858a1dd6580c1b9733c33566832a5fa11c5b6cf
Vagner Guedes <vagner.ads.guedes@gmail.com>
2022-03-15 11:36:29 -0300

    Lista de 10 inteiros

---
+++ estrutura-de-dados/src/br/ucsal/AtividadeArray.java
@@ -0,0 +1,20 @@
+package br.ucsal;
+
+public class AtividadeArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 10;
+		int vetor[]  = new int[n];
+		int i;
+		
+		for(i=0; i<n; i++) {
+	    vetor[i] = i+1;
+	    System.out.println(vetor[i]);
+			     
+		}
+
+	}
+
+}
