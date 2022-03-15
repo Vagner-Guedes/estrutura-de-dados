5688915c53d9f4a2c6fde06e2dd26ddc15a244ac
Vagner Guedes <vagner.ads.guedes@gmail.com>
2022-03-15 11:55:22 -0300

    adição de comentário final

--- estrutura-de-dados/src/br/ucsal/AtividadeArray.java
+++ estrutura-de-dados/src/br/ucsal/AtividadeArray.java
@@ -1,17 +1,20 @@
 package br.ucsal;
 
+// Listar 10 números inteiros com arrays
+
 public class AtividadeArray {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int n = 10;
+		int n = 10; 
 		int vetor[]  = new int[n];
 		int i;
 		
 		for(i=0; i<n; i++) {
 	    vetor[i] = i+1;
 	    System.out.println(vetor[i]);
+	    System.out.println("10 números inteiros com arrays listados");
 			     
 		}
 
